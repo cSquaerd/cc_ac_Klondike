9480f970d23bf5c4540bf67899b1797f7570327d
acochra7 <44787429+acochra7@users.noreply.github.com>
2018-11-30 12:08:31 -0500

    Updated the klondikeCarlo.
    Made it output as a percentage. Also, increased the number of trials to make it more accurate.

--- cc_ac_KlondikeModule.jl
+++ cc_ac_KlondikeModule.jl
@@ -174,12 +174,12 @@ module ccacKlondike
         false
     end
 
-    function klondikeCarlo(trials::Integer = 100_000, drawMode::Integer = 3)
+    function klondikeCarlo(trials::Integer = 10_000_000, drawMode::Integer = 3)
         local unplayables = 0
         for i = 1:trials
             local kb = KlondikeBoard()
             isPlayable(kb, drawMode) ? continue : unplayables += 1
         end
-        (unplayables, unplayables / trials)
+        (unplayables, (unplayables / trials)*100)
     end
 end
